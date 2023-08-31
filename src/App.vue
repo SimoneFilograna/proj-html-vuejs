54a286289413a9720f967fc8d33628accdc8b601
SimoneFilograna <simonefilogranaf12@gmail.com>
2023-08-31 16:29:06 +0200

    starting-project

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,21 @@
 <script>
+  import TheHeader from "./components/TheHeader.vue";
+  import TheMain from "./components/TheMain.vue";
+  import TheFooter from "./components/TheFooter.vue";
 
   export default{
-
+    components:{
+      TheHeader,
+      TheMain,
+      TheFooter
+    }
   }
 </script>
 
 <template>
-  
+  <TheHeader></TheHeader>
+  <TheMain></TheMain>
+  <TheFooter></TheFooter>
 </template>
 
 <style lang="scss">
---
+++ src/components/TheMain.vue
@@ -0,0 +1,13 @@
+<script>
+    export default{
+
+    }
+</script>
+
+<template>
+    <h1>main</h1>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
