740f1e88b7f5ef9d9c859983153b52f855dad594
SimoneFilograna <simonefilogranaf12@gmail.com>
2023-08-31 16:54:22 +0200

    add-mixins-bg

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -5,9 +5,28 @@
 </script>
 
 <template>
-    <h1>header</h1>
+    <header>
+        <div class="jumbotron">
+
+            <div class="container">
+                <div class="navbar">
+
+                </div>
+
+                <div class="introduce-container">
+
+                </div>
+
+            </div>
+        </div>
+    </header>
 </template>
 
 <style lang="scss" scoped>
+    @use "../sass/partials/mixins";
 
+    .jumbotron{
+        @include mixins.blue-background;
+        height: 200px;
+    }
 </style>
