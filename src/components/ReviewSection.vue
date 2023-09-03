531a026691a16627c7586ceeb56d40b2ef70a45e
SimoneFilograna <simonefilogranaf12@gmail.com>
2023-09-03 15:08:37 +0200

    fix-latest-post

--- src/components/ReviewSection.vue
+++ src/components/ReviewSection.vue
@@ -8,8 +8,8 @@
     <section>
         <div class="review-container">
             <div class="section-title-cont text-center">
-                <small class="d-inline-block pb-2 fw-medium">Blog</small>
-                <h2 class="fs-1 pb-2"><span class="fw-bold">Latest</span> Post</h2>
+                <small class="d-inline-block pb-3 fw-medium">Blog</small>
+                <h2 class="pb-2"><span class="fw-bold">Latest</span> Post</h2>
                 <p>When, while the lovely valley teems with vapour around meand the meridian sun strikes the upper surface</p>
             </div>
     
@@ -18,16 +18,18 @@
 
                     <!-- x2col card -->
 
-                    <div class="col-6">
-                        <div class="card h-100 text-white">                  
-                            <img src="../assets/headway-537308-unsplash-1380x703.jpg" class="card-img" alt="">
-                            <div class="card-img-overlay d-flex flex-column h-100 ps-5">
-                                <p class="card-text mt-auto">April 21, 2019 &bull; by Paul</p>
-                                <h5 class="card-title">Amazon gets CCI nod to acquire 49% stake in</h5>
-                                <div class="d-flex justify-content-between align-items-baseline">
-                                    <p>When, while the lovely valley teems with vapur aroud me, and the meridian s ...</p>
-                                    <button type="button" class="btn btn-light">Read More</button>
-                                </div>
+                    <div class="col-6 position-relative">
+                        <div class="filter-box"></div>
+                        <div class="h-100 text-white special-img-card d-flex align-items-end p-5 gap-5">
+
+                            <div class="card-body d-flex flex-column text-box position-relative gap-4">
+                                <p class="card-text">April 21, 2019 &bull; by Paul</p>
+                                <h5 class="card-title fw-bold fs-4">Clearing Corporations get SEBI not to invest in overnight funds</h5>
+                                <p class="card-text"> When, while the lovely valley teems with vapur aroud me, and the meridian s ...</p>
+                            </div>
+
+                            <div class="button position-relative">
+                                <button type="button" class="btn btn-light mt-auto">Read More</button>
                             </div>
                         </div>
                     </div>
@@ -36,9 +38,9 @@
 
                     <div class="col-3">
                         <div class="card h-100 small-card text-white">
-                            <div class="card-body d-flex flex-column mt-5 gap-5 px-5">
+                            <div class="card-body d-flex flex-column mt-5 gap-5 px-5 pb-5">
                                 <p class="card-text">April 21, 2019 &bull; by Paul</p>
-                                <h5 class="card-title">Clearing Corporations get SEBI not to invest in overnight funds</h5>
+                                <h5 class="card-title fw-bold fs-4">Clearing Corporations get SEBI not to invest in overnight funds</h5>
                                 <p class="card-text"> When, while the lovely valley teems with vapur aroud me, and the meridian s ...</p>
                                 <button type="button" class="btn btn-light mt-auto">Read More</button>
                             </div>
@@ -49,9 +51,9 @@
 
                     <div class="col-3">
                         <div class="card h-100 small-card text-white" >
-                            <div class="card-body d-flex flex-column mt-5 gap-5 px-5">
+                            <div class="card-body d-flex flex-column mt-5 gap-5 px-5 pb-5">
                                 <p class="card-text">April 21, 2019 &bull; by Paul</p>
-                                <h5 class="card-title">Shabnam Dhillion, wife of RSSB chief Gurinder Dhillion, passes away in London</h5>
+                                <h5 class="card-title fw-bold fs-4">Shabnam Dhillion, wife of RSSB chief Gurinder Dhillion, passes away in London</h5>
                                 <p class="card-text"> When, while the lovely valley teems with vapur aroud me, and the meridian s ...</p>
                                 <button type="button" class="btn btn-light mt-auto">Read More</button>
                             </div>
@@ -69,24 +71,57 @@
     @use "../sass/partials/variables";
 
     .review-container{
-        margin-top: 38rem;
+        margin-top: 40rem;
 
         .section-title-cont{
             small{
                 color: variables.$over-color-text;
-            }    
+                font-size: variables.$text-small-size;
+            };
+            
+            h2{
+                font-size: variables.$text-section-h2;
+            };
+
+            p{
+                font-size: variables.$text-small-size; 
+            }
         }
 
         .card-container{
             padding-top: 6rem;
+
+            .filter-box{
+                    
+                    position: absolute;
+                    border-radius: 30px;
+                    width: 95%;
+                    height: 100%;
+                    background-color: rgba(0, 0, 0, 0.349);
+                }
+            .special-img-card{
+                background-image: url(../assets/headway-537308-unsplash-1380x703.jpg);
+                border-radius: 30px;
+
+
+                .btn{
+                    width: 180px;
+                    border-radius: 40px;
+                    padding-top: .8rem;
+                    padding-bottom: .8rem;
+                    border: 0;
+
+                &:hover{
+                    @include mixins.red-hover;
+                    }
+                }   
+
+            }
             .card{
                 border-radius: 30px;
                 @include mixins.blue-background;
                 border: 0;
-                .card-img{
-                    border-radius: 30px;
-                    object-fit: contain;
-                }
+
                 .btn{
                     width: 180px;
                     border-radius: 40px;
