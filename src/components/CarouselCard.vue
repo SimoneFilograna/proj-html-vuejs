3a1af4f1402ae7cfe02617fb53299ba99b94c6e8
SimoneFilograna <simonefilogranaf12@gmail.com>
2023-09-03 11:00:38 +0200

    fixed-card-layout

--- src/components/CarouselCard.vue
+++ src/components/CarouselCard.vue
@@ -17,7 +17,7 @@
 </script>
 
 <template>
-    <div class="card h-100 d-flex flex-column " :key="carouselObj.id">
+    <div class="card h-100 d-flex flex-column me-3" :key="carouselObj.id">
         <img :src="getImg(carouselObj.img)" class="card-img-top" :alt="`card n${carouselObj.id}`">
         <div class="card-body d-flex justify-content-between">
             <span class="card-text">{{ carouselObj.title }}</span>
@@ -29,10 +29,10 @@
 <style lang="scss" scoped>
     @use "../sass/partials/mixins";
     .card{
-        width: 500px;
-        border-radius: 30px;
+        width: 550px;
+        border-radius: 35px;
         border: 1px;
-        box-shadow: 0px 0px 50px -20px rgb(88, 88, 88);
+        box-shadow: 0px 0px 30px -25px rgb(88, 88, 88);
 
     &:hover {
         @include mixins.red-hover;
@@ -41,6 +41,12 @@
         img{
             border-radius: 30px;
             border-color: transparent;
+        }
+
+        .card-body{
+            border-radius: 35px;
+            margin-top: 1rem;
+            margin-bottom: .8rem;
         }
     }
 
