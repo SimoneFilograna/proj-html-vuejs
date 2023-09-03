3f062aa272aa867ee73936e6cd7d726a8f015a37
SimoneFilograna <simonefilogranaf12@gmail.com>
2023-09-03 12:26:17 +0200

    add-button-fixed-button

--- src/components/FixedButton.vue
+++ src/components/FixedButton.vue
@@ -36,7 +36,11 @@
 
                 <!-- BOTTOM BUTTON -->
 
-    <div class="fixed-container-bottom">
+    <div class="fixed-container-bottom d-flex align-items-center gap-4">
+
+        <div class="arrow-container">
+            <i class="fa-solid fa-arrow-up"></i>
+        </div>
 
         <div class="chat-container text-white" >
             <i class="fa-regular fa-comment fa-flip-horizontal"></i>
@@ -83,8 +87,31 @@
     .fixed-container-bottom{
         z-index: 9999;
         position: fixed;
-        right: 4%;
+        right: 3%;
         bottom: 5%;
+
+        .arrow-container{
+            cursor: pointer;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            background-color: white;
+            border: 1px solid black;
+            border-radius: 50%;
+            width: 50px;
+            transition: background-color .5s cubic-bezier(0.47, 0, 0.745, 0.715);
+
+        &:hover{
+            background-color: #3d3d3d;
+            color: rgb(255, 255, 255);
+        }
+
+            i{
+                font-size: .9rem;
+                padding: 1rem;
+                
+            }
+        }
 
         .chat-container{
             cursor: pointer;
